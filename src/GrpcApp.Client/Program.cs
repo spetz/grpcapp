559f2437333885d033183eb0b8ff457faf2f9c55
Piotr Gankiewicz <spetz90@gmail.com>
2018-10-05 16:06:12 +0200

    Client

--- src/GrpcApp.Client/Program.cs
+++ src/GrpcApp.Client/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using GrpcApp.Core;
 
 namespace GrpcApp.Client
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string host = "localhost";
+            const int port = 50000;
+
+            var channel = new Channel($"{host}:{port}", ChannelCredentials.Insecure);
+            var client = new DemoService.DemoServiceClient(channel);
+
+            Console.WriteLine("Client has started...");
+
+            while (true)
+            {
+                Console.ReadKey();
+                await client.SendAsync(new Message
+                {
+                    Id = Guid.NewGuid().ToString("N"),
+                    User = "user #1",
+                    Content = "hello",
+                    Attachments =
+                    {
+                        new Attachment
+                        {
+                            Name = "Attachment #1"
+                        }
+                    }
+                });
+                Console.WriteLine("Message sent");
+            }
         }
     }
 }
