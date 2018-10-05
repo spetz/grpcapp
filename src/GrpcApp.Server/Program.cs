fa2cdc1c6d61c8777d5ec8cdb973a402b3d2ec6f
Piotr Gankiewicz <spetz90@gmail.com>
2018-10-05 16:01:32 +0200

    Server

--- src/GrpcApp.Server/Program.cs
+++ src/GrpcApp.Server/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using GrpcApp.Core;
 
 namespace GrpcApp.Server
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string host = "localhost";
+            const int port = 50000;
+
+            Console.WriteLine("Starting a server...");
+
+            var server = new Grpc.Core.Server
+            {
+                Services = { DemoService.BindService(new DemoServiceHost()) },
+                Ports = {new ServerPort(host, port, ServerCredentials.Insecure)}
+            };
+            
+            server.Start();
+
+            Console.WriteLine($"Listening on port {port}...");
+            Console.ReadKey();
+
+            await server.ShutdownAsync();
+        }
+    }
+
+    class DemoServiceHost : DemoService.DemoServiceBase
+    {
+        public override async Task<Empty> Send(Message request, ServerCallContext context)
+        {
+            await Task.CompletedTask;
+            Console.WriteLine($"Received a message: {request.Content} from: {context.Peer}");
+            
+            return new Empty();
         }
     }
 }
